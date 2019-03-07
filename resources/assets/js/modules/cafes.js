e5fd128db8058b1142a64d005d71f68aba4681c9
gdbiao <279535058@qq.com>
2019-03-07 17:47:00 +0800

    实现表单提交

--- resources/assets/js/modules/cafes.js
+++ resources/assets/js/modules/cafes.js
@@ -16,12 +16,30 @@ export const cafes = {
         cafesLoadStatus: 0,
 
         cafe: {},
-        cafeLoadStatus: 0
+        cafeLoadStatus: 0,
+
+        cafeAddStatus: 0
     },
     /**
      * Defines the actions used to retrieve the data.
      */
     actions: {
+        addCafe( { commit, state, dispatch }, data ){
+            // 状态1表示开始添加
+            commit( 'setCafeAddStatus', 1 );
+        
+            CafeAPI.postAddNewCafe( data.name, data.address, data.city, data.state, data.zip )
+                    .then( function( response ){
+                        // 状态2表示添加成功
+                        commit( 'setCafeAddStatus', 2 );
+                        dispatch( 'loadCafes' );
+                    })
+                    .catch( function(){
+                        // 状态3表示添加失败
+                        commit( 'setCafeAddStatus', 3 );
+                    });
+        },
+
         loadCafes( { commit } ){
             commit( 'setCafesLoadStatus', 1 );
 
@@ -69,6 +87,10 @@ export const cafes = {
 
         setCafe( state, cafe ){
             state.cafe = cafe;
+        },
+
+        setCafeAddStatus(state, status) {
+            state.cafeAddStatus = status;
         }
     },
     /**
@@ -89,6 +111,10 @@ export const cafes = {
 
         getCafe( state ){
             return state.cafe;
+        },
+
+        getCafeAddStatus( state) {
+            return state.cafeAddStatus;
         }
     }
 };
