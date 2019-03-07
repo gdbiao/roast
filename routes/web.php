f7732dec1dc2ec1bcaeffcae3ab8a86d1cefa93d
gdbiao <279535058@qq.com>
2019-03-07 14:27:06 +0800

    基于 Github 的用户认证

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 */
 
 Route::get('/', 'Web\AppController@getApp')->middleware('auth');
+Route::get('/login', 'Web\AppController@getLogin' )->name('login')->middleware('guest');
+Route::get( '/auth/{social}', 'Web\AuthenticationController@getSocialRedirect' )
+      ->middleware('guest');
+
+Route::get( '/auth/{social}/callback', 'Web\AuthenticationController@getSocialCallback' )
+      ->middleware('guest');
