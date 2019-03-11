3df8d37807c339660f8bf80ea17a2582ded36fd0
gdbiao <279535058@qq.com>
2019-03-11 15:01:46 +0800

    2

--- resources/assets/js/components/cafes/CafeMap.vue
+++ resources/assets/js/components/cafes/CafeMap.vue
@@ -1,3 +1,5 @@
+import {ROAST_CONFIG} from '../../config.js';
+
 <style lang="scss">
     div#cafe-map {
         width: 100%;
@@ -34,7 +36,10 @@
   }
 },
 data() {
-  return {}
+  return {
+      markers: [],
+      infoWindows: []
+  }
 },
 
 computed: {
@@ -57,26 +62,33 @@ this.buildMarkers();
 methods: {
    // 为所有咖啡店创建点标记
    buildMarkers() {
-       // 清空点标记数组
-       this.markers = [];
+    // 初始化点标记数组
+    this.markers = [];
 
-       // 遍历所有咖啡店并为每个咖啡店创建点标记
-       for (var i = 0; i < this.cafes.length; i++) {
+    // 自定义点标记图标
+    var image = ROAST_CONFIG.APP_URL + '/storage/img/coffee-marker.png';
+    var icon = new AMap.Icon({
+        image: image,  // 图像 URL
+        imageSize: new AMap.Size(19, 33)  // 设置图标尺寸
+    });
 
-           // 通过高德地图 API 为每个咖啡店创建点标记并设置经纬度
-           var marker = new AMap.Marker({
-               position: AMap.LngLat(parseFloat(this.cafes[i].latitude), parseFloat(this.cafes[i].longitude)),
-               title: this.cafes[i].name
-           });
+    // 遍历所有咖啡店创建点标记
+    for (var i = 0; i < this.cafes.length; i++) {
 
-           // 将每个点标记放到点标记数组中
-           this.markers.push(marker);
-       }
+        // 为每个咖啡店创建点标记并设置经纬度
+        var marker = new AMap.Marker({
+            position: AMap.LngLat(parseFloat(this.cafes[i].latitude), parseFloat(this.cafes[i].longitude)),
+            title: this.cafes[i].name,
+            icon: icon,  // 通过 icon 对象设置自定义点标记图标来替代默认蓝色图标
+        });
 
-       // 将所有点标记显示到地图上
-       this.map.add(this.markers);
-   },
-   // 从地图上清理点标记
+        // 将点标记放到数组中
+        this.markers.push(marker);
+    }
+
+    // 将所有点标记显示到地图上
+    this.map.add(this.markers);
+},
 clearMarkers() {
     // 遍历所有点标记并将其设置为 null 从而从地图上将其清除
     for (var i = 0; i < this.markers.length; i++) {
